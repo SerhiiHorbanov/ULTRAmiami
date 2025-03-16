c412c1947f26a37ba969dfc5d89d143d2b6a2f9f
Serhiiko <serikgorbanev@gmail.com>
2025-03-16 19:51:12 +0100

    Added UnitController

---
+++ Scripts/Controllers/UnitController.cs
@@ -0,0 +1,22 @@
+using Godot;
+using ULTRAmiami.Units;
+
+namespace ULTRAmiami.Controllers;
+
+public abstract partial class UnitController : Node
+{
+	[Export] protected Unit Unit;
+	
+	public override void _Process(double delta)
+	{
+		UpdateTargetDirection();
+	}
+	
+	protected void UpdateTargetDirection()
+	{
+		Vector2 targetDirection = GetTargetDirection();
+		Unit.SetTargetDirection(targetDirection);
+	}
+	
+	protected abstract Vector2 GetTargetDirection();
+}
