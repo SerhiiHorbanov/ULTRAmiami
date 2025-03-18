53f370fd5e46af06cbbc1d698065816bb3dc3aeb
Serhiiko <serikgorbanev@gmail.com>
2025-03-18 17:06:47 +0100

    Added GetChild<T> to NodeTreeHelper

--- Scripts/NodeTreeHelper.cs
+++ Scripts/NodeTreeHelper.cs
@@ -4,6 +4,14 @@ namespace ULTRAmiami;
 
 public static class NodeTreeHelper
 {
+	public static T GetChild<T>(this Node node) where T : Node
+	{
+		foreach (Node child in node.GetChildren())
+			if (child is T t)
+				return t;
+		return null;
+	}
+	
 	public static T GetAncestor<T>(this Node node) where T : Node
 	{
 		while (node != null)
