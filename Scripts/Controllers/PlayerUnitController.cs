9eaa76c9e360cc7d2ec3c816353573b9872c00f5
Serhiiko <serikgorbanev@gmail.com>
2025-03-18 17:19:32 +0100

    Implemented use of Weapon to Player and PlayerUnitController

--- Scripts/Controllers/PlayerUnitController.cs
+++ Scripts/Controllers/PlayerUnitController.cs
@@ -4,6 +4,16 @@ namespace ULTRAmiami.Controllers;
 
 public partial class PlayerUnitController : UnitController
 {
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		
+		if (Input.IsActionJustPressed("shoot"))
+		{
+			Unit.Weapon?.Shoot();
+		}
+	}
+	
 	protected override Vector2 GetTargetDirection()
 	{
 		return Input.GetVector("left", "right", "up", "down");
