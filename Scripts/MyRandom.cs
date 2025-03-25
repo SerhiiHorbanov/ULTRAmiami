9ca15e57e6b7c4cf11cd54a26da740a9166b6bce
Serhiiko <serikgorbanev@gmail.com>
2025-03-25 17:55:55 +0100

    Added MyRandom

--- Scripts/MyRandom.cs
+++ Scripts/MyRandom.cs
@@ -1,6 +1,15 @@
+using System;
+using Godot;
+
 namespace ULTRAmiami;
 
-public class MyRandom
+public static class MyRandom
 {
-    
+    public static Vector2 RandomVectorInRange(this Vector2 origin, Vector2 range)
+    {
+        float xOffset = Random.Shared.NextSingle() * range.X * 2 - range.X;
+        float yOffset = Random.Shared.NextSingle() * range.Y * 2 - range.Y;
+
+        return new(origin.X + xOffset, origin.Y + yOffset);
+    }
 }
