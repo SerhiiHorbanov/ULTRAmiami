80e9ff2392ed01920de4074f6de6397ec4748079
Serhiiko <serikgorbanev@gmail.com>
2025-03-31 20:56:51 +0200

    Added WithLimitedLength to MyMath

--- Scripts/MyMath.cs
+++ Scripts/MyMath.cs
@@ -6,4 +6,7 @@ public static class MyMath
 {
     public static Vector2 RotateAround(this Vector2 point, Vector2 axis, float radians)
         => (point - axis).Rotated(radians) + axis;
+    
+    public static Vector2 WithLimitedLength(this Vector2 vector, float limit)
+        => vector.LengthSquared() > limit * limit ? vector.Normalized() * limit : vector;
 }
