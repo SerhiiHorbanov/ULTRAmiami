26ea007780e020b403e9a34eccccc46c66e9ba45
Serhiiko <serikgorbanev@gmail.com>
2025-04-04 23:37:40 +0200

    Added CosineSimilarity to MyMath

--- Scripts/MyMath.cs
+++ Scripts/MyMath.cs
@@ -9,4 +9,7 @@ public static class MyMath
     
     public static Vector2 WithLimitedLength(this Vector2 vector, float limit)
         => vector.LengthSquared() > limit * limit ? vector.Normalized() * limit : vector;
+    
+    public static float CosineSimilarity(this Vector2 vector, Vector2 other)
+        => vector.Normalized().Dot(other.Normalized());
 }
