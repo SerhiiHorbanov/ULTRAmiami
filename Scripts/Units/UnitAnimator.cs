39484e56c97a7dc5bc599448aed6660fb6ebdcab
Serhiiko <serikgorbanev@gmail.com>
2025-04-20 22:00:07 +0200

    Added animations to Unit
    For now all units use pistol enemy's animation

---
+++ Scripts/Units/UnitAnimator.cs
@@ -0,0 +1,41 @@
+using Godot;
+using ULTRAmiami.Weapons;
+
+namespace ULTRAmiami.Units;
+
+public partial class UnitAnimator : Node2D
+{
+	[Export] private Unit _unit;
+	[Export] private Sprite2D _bodySprite;
+	[Export] private AnimatedSprite2D _legs;
+
+	private Weapon Weapon
+		=> _unit.Weapon;
+	
+	private bool IsWalking
+		=> _unit.TargetDirection != Vector2.Zero;
+	
+	public override void _Process(double delta)
+	{
+		UpdateRotation();
+		UpdateAnimation();
+	}
+
+	private void UpdateAnimation()
+	{
+		if (IsWalking)
+			_legs.Play();
+		else
+			_legs.Stop();
+	}
+
+	private void UpdateRotation()
+	{
+		_bodySprite.Rotation = Weapon.RelativePointingAt.Angle();
+		
+		if (IsWalking)
+			_legs.Rotation = _unit.TargetDirection.Angle();
+		else
+			_legs.Rotation = _bodySprite.Rotation;
+	}
+}
