d4c4fea9e1d270792d32b267223204c3bd2bd251
Serhiiko <serikgorbanev@gmail.com>
2025-04-22 16:44:22 +0200

    Addded MusicManager

---
+++ Scripts/MusicManager.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+namespace ULTRAmiami;
+
+public partial class MusicManager : AudioStreamPlayer
+{
+	[Export] private AudioEffect _onDeathEffect;
+
+	private int BusIdx;
+	
+	public override void _Ready()
+	{
+		BusIdx = AudioServer.GetBusIndex(Bus);
+		
+		if (AudioServer.GetBusEffectCount(BusIdx) > 0)
+			AudioServer.RemoveBusEffect(BusIdx, 0);
+	}
+
+	private void OnPlayerDeath()
+	{
+		AudioServer.AddBusEffect(BusIdx, _onDeathEffect);
+	}
+}
