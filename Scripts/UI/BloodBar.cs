80f53f6ec9e8e386f97906c15b8422fb27a268c9
Serhiiko <serikgorbanev@gmail.com>
2025-04-22 10:27:19 +0200

    Doubled BloodBar to left side

--- Scripts/UI/BloodBar.cs
+++ Scripts/UI/BloodBar.cs
@@ -5,18 +5,26 @@ namespace ULTRAmiami.UI;
 public partial class BloodBar : Control
 {
 	[Export] private Control _fill;
+	[Export] private Control _fill2;
 	[Export] private float _max;
 	private float _maxHeight;
+	private float _maxHeight2;
 
 	public override void _Ready()
 	{
 		_maxHeight = _fill.Size.Y;
+		_maxHeight2 = _fill2.Size.Y;
 	}
 
 	private void SetBlood(float blood)
 	{
 		float height = _maxHeight * blood / _max;
+		float height2 = _maxHeight * blood / _max;
+		
 		height = float.Clamp(height, 0, _maxHeight);
+		height2 = float.Clamp(height2, 0, _maxHeight2);
+		
 		_fill.Size = new(_fill.Size.X, height);
+		_fill2.Size = new(_fill2.Size.X, height2);
 	}
 }
