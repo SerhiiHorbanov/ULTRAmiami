de62fc902ce295ec164491d7b74f03a43012ecfe
Serhiiko <serikgorbanev@gmail.com>
2025-05-06 00:17:29 +0200

    Added IntersectRayWithLine method for MyMath

--- Scripts/Utils/MyMath.cs
+++ Scripts/Utils/MyMath.cs
@@ -12,4 +12,23 @@ public static class MyMath
     
     public static float CosineSimilarity(this Vector2 vector, Vector2 other)
         => vector.Normalized().Dot(other.Normalized());
+    
+    public static Vector2? IntersectRayWithLine(Vector2 rayOrigin, Vector2 rayDir, Vector2 linePoint, Vector2 lineDir)
+    {
+        float determinant = rayDir.X * lineDir.Y - rayDir.Y * lineDir.X;
+
+        // If determinant is 0, the lines are parallel
+        if (Mathf.Abs(determinant) < Mathf.Epsilon)
+            return null;
+
+        Vector2 diff = linePoint - rayOrigin;
+
+        float t = (diff.X * lineDir.Y - diff.Y * lineDir.X) / determinant;
+
+        if (t < 0)
+            return null; // Intersection is behind the ray's origin
+
+        Vector2 intersection = rayOrigin + t * rayDir;
+        return intersection;
+    }
 }
