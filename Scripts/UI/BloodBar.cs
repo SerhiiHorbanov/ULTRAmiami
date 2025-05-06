2df3188f08cb33f395a94452971ebf1fe0a255bc
Serhiiko <serikgorbanev@gmail.com>
2025-05-06 16:29:18 +0200

    Added color change to blood bar when losing blood or getting blood

--- Scripts/UI/BloodBar.cs
+++ Scripts/UI/BloodBar.cs
@@ -4,31 +4,87 @@ namespace ULTRAmiami.UI;
 
 public partial class BloodBar : Control
 {
-	[Export] private Control _fill;
-	[Export] private Control _fill2;
+	[Export] private Color _goingUpColor;
+	
+	[Export] private Gradient _defaultColor;
+	[Export] private float _bloodDifferenceForGradientEndColor;
+	
+	[Export] private ColorRect _fill;
+	[Export] private ColorRect _fill2;
 	[Export] private float _min;
 	[Export] private float _max;
 	private float _maxHeight;
 	private float _maxHeight2;
 
+	private float _previousBlood;
+
+	[Export] private int _deltaSamplesAmount = 60;
+	private float[] _bloodDeltaSamples;
+	private int _oldestSampleIndex;
+
 	public override void _Ready()
 	{
+		_bloodDeltaSamples = new float[_deltaSamplesAmount];
+		
 		_maxHeight = _fill.Size.Y;
 		_maxHeight2 = _fill2.Size.Y;
 	}
 
 	private void SetBlood(float blood)
 	{
-		float height = GetHeight(blood, _maxHeight);
-		float height2 = GetHeight(blood, _maxHeight2);
+		if (_previousBlood == 0)
+			_previousBlood = blood;
 		
-		height = float.Clamp(height, 0, _maxHeight);
-		height2 = float.Clamp(height2, 0, _maxHeight2);
+		UpdateBloodDeltaSamples(blood);
 		
-		_fill.Size = new(_fill.Size.X, height);
-		_fill2.Size = new(_fill2.Size.X, height2);
+		Color color = DecideCurrentColor(blood);
+		
+		_fill.Color = color;
+		_fill2.Color = color;
+		
+		UpdateHeightForFill(_fill, blood, _maxHeight);
+		UpdateHeightForFill(_fill2, blood, _maxHeight2);
+		
+		_previousBlood = blood;
+	}
+	
+	private void UpdateBloodDeltaSamples(float blood)
+	{
+		_bloodDeltaSamples[_oldestSampleIndex] = blood - _previousBlood;
+		_oldestSampleIndex++;
+		_oldestSampleIndex %= _deltaSamplesAmount;
+	}
+
+	private void UpdateHeightForFill(ColorRect fill, float blood, float maxHeight)
+	{
+		float height = GetHeight(blood, maxHeight);
+		fill.Size = new(_fill.Size.X, height);
+	}
+
+	private float CalculateAverageBloodDelta()
+	{
+		float sum = 0;
+
+		foreach (float delta in _bloodDeltaSamples)
+			sum += delta;
+		
+		return sum / _deltaSamplesAmount;
+	}
+	
+	private Color DecideCurrentColor(float blood)
+	{
+		float averageDelta = CalculateAverageBloodDelta();
+		
+		if (averageDelta > 0)
+			return _goingUpColor;
+		
+		float t = -averageDelta / _bloodDifferenceForGradientEndColor;
+		return _defaultColor.Sample(t);
 	}
 
 	private float GetHeight(float blood, float maxHeight)
-		=> maxHeight * (blood - _min) / (_max - _min);
+	{
+		float height = maxHeight * (blood - _min) / (_max - _min);
+		return float.Clamp(height, 0, maxHeight);
+	}
 }
