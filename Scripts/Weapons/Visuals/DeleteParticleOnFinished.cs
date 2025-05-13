c1044a49ec132011198c98d6434db181e1187210
Serhiiko <serikgorbanev@gmail.com>
2025-05-13 12:54:08 +0200

    Removed deprecated script

--- Scripts/Weapons/Visuals/DeleteParticleOnFinished.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-namespace ULTRAmiami.Weapons.Visuals;
-
-public partial class DeleteParticleOnFinished : CpuParticles2D
-{
-	public override void _Ready()
-		=> Finished += QueueFree;
-}
