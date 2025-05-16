cea15171750a117a5cbff22d9e60e09e0e5afc06
Serhiiko <serikgorbanev@gmail.com>
2025-05-16 23:11:23 +0200

    Updated TestEnemySpawner

--- Scripts/Debug/TestEnemySpawner.cs
+++ Scripts/Debug/TestEnemySpawner.cs
@@ -6,25 +6,12 @@ namespace ULTRAmiami.Debug;
 public partial class TestEnemySpawner : Node
 {
     [Export] private PackedScene _spawnedEnemyPackedScene;
-    [Export] private Unit _player;
     [Export] private Timer _timer;
     [Export] private Node2D _spawnPosition;
     
     public override void _Ready()
     {
         _timer.Timeout += OnTimerTimeout;
-        _player.OnDeath += OnPlayerDeath;
-    }
-
-    public override void _ExitTree()
-    {
-        if (_player is not null)
-            _player.OnDeath -= OnPlayerDeath;
-    }
-
-    private void OnPlayerDeath(Hit hit)
-    {
-        _player = null;
     }
 
     private void OnTimerTimeout()
