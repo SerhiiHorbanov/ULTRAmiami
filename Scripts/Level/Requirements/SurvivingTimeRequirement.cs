f7df98b7d7c4c5ae1cb7d031c7c05cae65f08f44
Serhiiko <serikgorbanev@gmail.com>
2025-05-17 00:45:00 +0200

    Added SurvivingTimeRequirement

---
+++ Scripts/Level/Requirements/SurvivingTimeRequirement.cs
@@ -0,0 +1,44 @@
+using Godot;
+using ULTRAmiami.Units;
+
+namespace ULTRAmiami.Level.Requirements;
+
+public partial class SurvivingTimeRequirement : CompletionRequirement
+{
+	private bool _isDead;
+	private bool _timeIsOut;
+	private bool _isCompleted;
+	
+	[Export] private Timer _timer;
+	[Export] private Unit _player;
+	
+	public override void _Ready()
+	{
+		_timer.Timeout += Timeout;
+		_player.OnDeath += OnDeath;
+		_timer.Start();
+	}
+
+	public override void _Process(double delta)
+	{
+		GD.Print(_timer.TimeLeft);
+	}
+
+	private void OnDeath(Hit hit)
+	{
+		_isDead = true;
+		_player.OnDeath -= OnDeath;
+	}
+
+	private void Timeout()
+	{
+		_timeIsOut = true;
+		_isCompleted = !_isDead;
+	}
+	
+	public override bool IsCompleted()
+		=> _isCompleted;
+
+	public override bool IsFailed()
+		=> _isDead && !_timeIsOut && !_isCompleted;
+}
