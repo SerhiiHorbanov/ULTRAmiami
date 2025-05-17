80ccece6504123ae6145cc7f47533a7f3a4ac5c6
Serhiiko <serikgorbanev@gmail.com>
2025-05-17 10:37:05 +0200

    Added TriggerableDoor

---
+++ Scripts/Level/Features/TriggerableDoor.cs
@@ -0,0 +1,28 @@
+using Godot;
+
+namespace ULTRAmiami.Level.Features;
+
+public partial class TriggerableDoor : Node2D
+{
+	[Export] private int _triggersNeeded;
+
+	[Export] private AnimationPlayer _animationPlayer;
+	[Export] private StringName _openingAnimationName;
+	
+	public void Trigger()
+	{
+		if (_triggersNeeded == 0)
+			return;
+		
+		_triggersNeeded--;
+
+		if (_triggersNeeded == 0)
+			Open();
+	}
+
+	public void Open()
+	{
+		_triggersNeeded = 0;
+		_animationPlayer.Play(_openingAnimationName);
+	}
+}
