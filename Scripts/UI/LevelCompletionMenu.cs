b8661a6e1cbab883d87bf1870f502e8776e47354
Serhiiko <serikgorbanev@gmail.com>
2025-05-20 15:08:04 +0200

    Added LevelCompletionMenu

---
+++ Scripts/UI/LevelCompletionMenu.cs
@@ -0,0 +1,25 @@
+using Godot;
+using ULTRAmiami.Data;
+
+namespace ULTRAmiami.UI;
+
+public partial class LevelCompletionMenu : Control
+{
+	[Export] private RichTextLabel _levelNameLabel;
+	
+	[Export] private RichTextLabel _killsLabel;
+	[Export] private RichTextLabel _bloodConsumedLabel;
+	
+	private LevelInfo _info;
+	
+	public void Display(LevelInfo levelInfo, PlayerScore playerScore)
+	{
+		Visible = true;
+		
+		_info = levelInfo;
+		
+		_levelNameLabel.Text = levelInfo.LevelName;
+		_killsLabel.Text = playerScore.Kills.ToString();
+		_bloodConsumedLabel.Text = float.Round(playerScore.BloodConsumed, 1).ToString();
+	}
+}
