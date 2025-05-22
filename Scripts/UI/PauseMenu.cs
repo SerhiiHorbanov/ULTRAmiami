03c41099e625da73ae38d41ad83e8b6a7a200f4f
Serhiiko <serikgorbanev@gmail.com>
2025-05-22 17:54:29 +0200

    Added PauseMenu

---
+++ Scripts/UI/PauseMenu.cs
@@ -0,0 +1,61 @@
+using Godot;
+
+namespace ULTRAmiami.UI;
+
+public partial class PauseMenu : Control
+{
+	private readonly static StringName Escape = "escape";
+
+	[Export] private PackedScene _sceneExitingTo;
+
+	[Export] private bool _isPaused;
+
+	[Signal]
+	public delegate void OnExitingEventHandler();
+	
+	[Signal]
+	public delegate void OnRestartLevelEventHandler();
+	
+	[Signal]
+	public delegate void OnNextLevelEventHandler();
+	
+	public override void _Ready()
+	{
+		SetPaused(_isPaused);
+	}
+	
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event.IsActionPressed(Escape))
+		{
+			SetPaused(!_isPaused);
+		}
+	}
+
+	private void SetPaused(bool paused)
+	{
+		Visible = paused;
+		_isPaused = paused;
+		GetTree().Paused = paused;
+	}
+	
+	private void Resume()
+		=> SetPaused(false);
+
+	private void Settings()
+	{
+	}
+	
+	private void RestartLevel()
+		=> EmitSignalOnRestartLevel();
+
+	private void NextLevel()
+		=> EmitSignalOnNextLevel();
+	
+	private void Exit()
+	{
+		SetPaused(false);
+		GetTree().ChangeSceneToPacked(_sceneExitingTo);
+		EmitSignalOnExiting();
+	}
+}
