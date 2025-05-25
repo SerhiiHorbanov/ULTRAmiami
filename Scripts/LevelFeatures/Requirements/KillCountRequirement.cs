32a9ba01996ca13e90eced992c7866226d8dc0fe
Serhiiko <serikgorbanev@gmail.com>
2025-05-25 19:08:41 +0200

    Added KillCountRequirementDisplay

--- Scripts/LevelFeatures/Requirements/KillCountRequirement.cs
+++ Scripts/LevelFeatures/Requirements/KillCountRequirement.cs
@@ -1,5 +1,6 @@
 using Godot;
 using ULTRAmiami.Data;
+using ULTRAmiami.UI.LevelRequirementsUI;
 
 namespace ULTRAmiami.Requirements;
 
@@ -9,9 +10,41 @@ public partial class KillCountRequirement : CompletionRequirement
 	[Export] private int _maxKills = UnlimitedMaxKills;
 	
 	private const int UnlimitedMaxKills = -1;
+
+	private uint _prevKills;
 	
 	private uint Kills
 		=> PlayerScore.Current.Kills;
+
+	[Signal]
+	public delegate void OnKillsChangedEventHandler(uint newValue);
+	
+	public override void _Process(double delta)
+	{
+		if (Kills == _prevKills)
+			return;
+		
+		EmitSignalOnKillsChanged(Kills);
+		_prevKills = Kills;
+	}
+
+	public override Node InstantiateAndConnectToUI()
+	{
+		Node ui = InstantiateUI();
+
+		if (ui is not KillCountRequirementDisplay killCountRequirementDisplay)
+		{
+			GD.PrintErr("KillCountRequirementDisplay is not a KillCountRequirementDisplay");
+			return null;
+		}
+		
+		killCountRequirementDisplay.Initialize(_minKills, _maxKills);
+		OnKillsChanged += killCountRequirementDisplay.UpdateText;
+		
+		EmitSignalOnKillsChanged(Kills);
+		
+		return ui;
+	}
 
 	public override bool IsCompleted()
 		=> Kills >= _minKills && (_maxKills == UnlimitedMaxKills || Kills <= _maxKills);
---
+++ Scripts/UI/LevelRequirementsUI/KillCountRequirementDisplay.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+namespace ULTRAmiami.UI.LevelRequirementsUI;
+
+public partial class KillCountRequirementDisplay : Control
+{
+	[Export] private RichTextLabel _label;
+	[Export] private string _prefix = "";
+
+	private int _min;
+	private int _max;
+	
+	public void Initialize(int minKills, int maxKills)
+	{
+		_min = minKills;
+		_max = maxKills;
+	}
+	
+	public void UpdateText(uint killsAmount)
+	{
+		string firstNumberColor = KillsReachedMin(killsAmount) ? "[color=green]" : "[color=yellow]";
+		string secondNumberColor;
+		
+		if (IsUnlimitedMaxKills(_max))
+			secondNumberColor = $"[color=green]{_min}";
+		else
+			secondNumberColor = KillsReachedMin(killsAmount) ? $"[color=red]{_max}" : $"[color=green]{_min}";
+
+		string firstNumber = killsAmount.ToString();
+		
+		_label.Text = _prefix + firstNumberColor + firstNumber + "[color=white]/" + secondNumberColor;
+	}
+	
+	private bool KillsReachedMin(uint killsAmount)
+		=> killsAmount >= _min;
+
+	private static bool IsUnlimitedMaxKills(int maxKills)
+		=> maxKills == -1;
+}
