1e63b041aa51ee6857a4f9b50fb006651efda26f
Serhiiko <serikgorbanev@gmail.com>
2025-05-26 20:12:52 +0200

    Added AreaTrigger

---
+++ Scripts/LevelFeatures/AreaTrigger.cs
@@ -0,0 +1,36 @@
+using Godot;
+using Godot.Collections;
+using ULTRAmiami.Units;
+
+namespace ULTRAmiami;
+
+public partial class AreaTrigger : Area2D
+{
+	[Export] private bool _activatedOnce = true;
+	[Export] private bool _activatedOnlyByPlayer = true;
+	
+	[Signal]
+	public delegate void OnTriggeringEventHandler();
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body is not Unit unit)
+			return;
+		
+		if (_activatedOnlyByPlayer)
+		{
+			if (!unit.IsPlayer)
+				return;
+		}
+		
+		Trigger();
+	}
+	
+	private void Trigger()
+	{
+		if (_activatedOnce)
+			BodyEntered -= OnBodyEntered;
+		
+		EmitSignalOnTriggering();
+	}
+}
