36bf8d6b4ce7d0661f3f5b0c57e5b2c7d60a06d2
Serhiiko <serikgorbanev@gmail.com>
2025-06-03 13:27:55 +0200

    Added PlayInfiniteButton to main menu

--- Scripts/UI/MainMenu/MainMenuButtons.cs
+++ Scripts/UI/MainMenu/MainMenuButtons.cs
@@ -5,10 +5,16 @@ namespace ULTRAmiami.UI.MainMenu;
 public partial class MainMenuButtons : Node
 {
 	[Export] private Control _levelSelectionMenu;
+	[Export(PropertyHint.File, "*.tscn")] private string _infiniteModeScenePath;
 	
-	private void Play()
+	private void SelectLevel()
 	{
 		_levelSelectionMenu.Visible = true;
+	}
+
+	private void PlayInfinite()
+	{
+		GetTree().ChangeSceneToFile(_infiniteModeScenePath);
 	}
 
 	private void ExitGame()
