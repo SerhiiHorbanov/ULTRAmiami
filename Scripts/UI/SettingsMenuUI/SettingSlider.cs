69f95ad898f203f815eaba87e78e7eff631f84bc
Serhiiko <serikgorbanev@gmail.com>
2025-06-07 10:49:23 +0200

    Added value text to setting slider

--- Scripts/UI/SettingsMenuUI/SettingSlider.cs
+++ Scripts/UI/SettingsMenuUI/SettingSlider.cs
@@ -10,14 +10,26 @@ public partial class SettingSlider : Control
 	[Export(PropertyHint.Dir)] private string _path;
 	[Export] private HSlider _slider;
 
+	[Export(PropertyHint.Enum)] private SliderValueTextType _textType;
+	[Export] private Label _valueLabel;
+	
 	[Signal]
 	public delegate void OnValueChangedEventHandler(float value);
+
+	private enum SliderValueTextType
+	{
+		Percentage,
+		ValueInt,
+		ValueFloat,
+	}
 	
 	public override void _Ready()
 	{
 		_data.Load(GetSaveFilePath());
 		EmitSignalOnValueChanged(_data.Value);
 		_slider.Value = _data.Value;	
+		
+		UpdateValueText(_data.Value);
 	}
 
 	private string GetSaveFilePath()
@@ -38,5 +50,16 @@ public partial class SettingSlider : Control
 	{
 		_data.Value = value;
 		EmitSignalOnValueChanged(value);
+		UpdateValueText(value);
+	}
+	
+	private void UpdateValueText(float value)
+	{
+		_valueLabel.Text = _textType switch
+		{
+			SliderValueTextType.Percentage => $"{(int)(value * 100)}%",
+			SliderValueTextType.ValueInt => $"{(int)value}",
+			SliderValueTextType.ValueFloat => $"{value:F2}",
+		};
 	}
 }
