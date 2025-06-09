ea4fea4b61d67cea2eebb351c41734a4190f4702
Serhiiko <serikgorbanev@gmail.com>
2025-06-09 23:15:52 +0200

    Added more effects to blood
    Right now it's very unoptimized, so I will optimize it

--- Scripts/Effects/BloodSplatter.cs
+++ Scripts/Effects/BloodSplatter.cs
@@ -9,8 +9,14 @@ public partial class BloodSplatter : Node2D
 {
 	[Export] private float _scaleRandomization;
 	[Export] private float _animationSpeedScaleRandomization;
-
+	
 	[Export] private AnimatedSprite2D _animation;
+
+	[Export] private float _bloodColorRandomization;
+	[Export] private Timer _bloodDryingTimer;
+	[Export] private Color _initialBloodColor;
+	[Export] private Color _dryBloodColor;
+	[Export] private Node2D _sprite;
 	
 	public void Initialize(Unit bleeder, Hit hit)
 	{
@@ -27,5 +33,37 @@ public partial class BloodSplatter : Node2D
 		
 		Scale *= 1 + MyRandom.Range(_scaleRandomization);
 		_animation.SpeedScale *= 1 + MyRandom.Range(_animationSpeedScaleRandomization);
+		
+		RandomizeColor();
+	}
+	private void RandomizeColor()
+	{
+		float r = MyRandom.Range(1 - _bloodColorRandomization, 1);
+		
+		float a = _initialBloodColor.A;
+		_initialBloodColor *= r;
+		_initialBloodColor.A = a;
+		
+		a = _dryBloodColor.A;
+		_dryBloodColor *= r;
+		_dryBloodColor.A = a;
+		
+		Modulate = _initialBloodColor;
+	}
+
+	public override void _Process(double delta)
+	{
+		if (_bloodDryingTimer.IsStopped())
+			return;
+	
+		float t = 1 - (float)(_bloodDryingTimer.TimeLeft / _bloodDryingTimer.WaitTime);
+		Modulate = _initialBloodColor.Lerp(_dryBloodColor, t);
+	}
+
+	private void Simplify()
+	{
+		_sprite.MakeSiblingOf(this);
+		_sprite.Modulate = Modulate;
+		QueueFree();
 	}
 }
