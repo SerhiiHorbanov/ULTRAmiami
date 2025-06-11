a52445846ffc07b62293366882742451aa0b4d7a
Serhiiko <serikgorbanev@gmail.com>
2025-06-11 20:04:13 +0200

    Assigned new breakable trigger textures

--- Scripts/LevelFeatures/BreakableTrigger.cs
+++ Scripts/LevelFeatures/BreakableTrigger.cs
@@ -6,9 +6,6 @@ namespace ULTRAmiami;
 public partial class BreakableTrigger : Node2D, IAttackable
 {
 	private bool _isBroken;
-	
-	[Export] private Node2D _notBrokenGraphics;
-	[Export] private Node2D _brokenGraphics;
 
 	public bool Bleeds
 		=> false;
@@ -16,19 +13,12 @@ public partial class BreakableTrigger : Node2D, IAttackable
 	[Signal]
 	public delegate void OnBrokenEventHandler();
 
-	public override void _Ready()
-	{
-		RemoveChild(_brokenGraphics);
-	}
-
 	public void Hit(Hit hit)
 	{
 		if (_isBroken)
 			return;
 		
 		_isBroken = true;
-		_notBrokenGraphics.QueueFree();
-		AddChild(_brokenGraphics);
 		EmitSignalOnBroken();
 	}
 }
