1fdc248a6eb9b549c3e67dd94b3a74b73a9e0658
Serhiiko <serikgorbanev@gmail.com>
2025-06-14 00:27:34 +0200

    Added curves to death camera

--- Scripts/DeathCamera.cs
+++ Scripts/DeathCamera.cs
@@ -8,24 +8,44 @@ public partial class DeathCamera : Camera2D
 	[Export] private float _rotationSpeedDeg;
 	[Export] private float _speed;
 	[Export] private float _zoomSpeed;
+
+	[Export] private Timer _curveTimer;
+	[Export] private Curve _speedCurve;
+	[Export] private Curve _rotationCurve;
+	[Export] private Curve _zoomCurve;
+
+	private float _currentZoomSpeed;
 	
 	private float _deltaRotationRad;
+	
+	private Vector2 _direction;
 	private Vector2 _velocity;
 
 	public void Initialize(Vector2 globalPosition, Hit killingHit)
 	{
 		GlobalPosition = globalPosition;
-		_velocity = killingHit.Force.Normalized() * _speed;
+		_direction = killingHit.Force.Normalized();
+	}
+
+	private void UpdateValuesByCurves()
+	{
+		float t = 1 - (float)(_curveTimer.TimeLeft / _curveTimer.WaitTime);
+		_velocity = _direction * _speed * _speedCurve.Sample(t);
+		_currentZoomSpeed = _zoomSpeed * _zoomCurve.Sample(t);
 		
 		float rotationSpeedRad = float.DegreesToRadians(_rotationSpeedDeg);
-		_deltaRotationRad = killingHit.Force.X > 0 ? rotationSpeedRad : -rotationSpeedRad;
+		_deltaRotationRad = rotationSpeedRad * _rotationCurve.Sample(t);
+		if (_direction.X < 0)
+			_deltaRotationRad = -_deltaRotationRad;
 	}
 	
 	public override void _Process(double delta)
 	{
 		float d = (float)delta;
 		
-		Zoom += Vector2.One * _zoomSpeed * d;
+		UpdateValuesByCurves();
+		
+		Zoom += Vector2.One * _currentZoomSpeed * d;
 		Position += _velocity * d;
 		Rotation += _deltaRotationRad * d;
 	}
